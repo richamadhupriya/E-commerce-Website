75860edae0252cb1fdee2b779958ce6b94355647
richamadhupriya <richa.madhupriya@outlook.com>
2018-10-23 16:57:42 +0530

    Add files via upload

--- E-commerce-Website-master/js/cart.js
+++ E-commerce-Website-master/js/cart.js
@@ -2,51 +2,62 @@ var cart = [];
 var wishlist = [];
 
 /*
-    desc: Adding to cart
+    @desc: display cart
 */
-function addToCart(id)
-{
-    $.ajax({
-        type: "GET",
-        data : "json",
-        url: "../data/productData.json",
-        success : function(result){
-            for (var product of result)
-            {
-                if(product.id == id)
-                {
-                    cart.push(product);
-                    alert("Added to cart");
-
-                }
+function displayCart() {
+    //console.log(localStorage.wishlist);
+    $.getJSON("../data/productData.json", function (result) {
+        $.each(result.Products, function (data, cartItem) {
+            if (localStorage.cart == cartItem.id) {
+                cart.push(cartItem);
+                console.log(cart);
             }
-        },
-        error : function(error){
-            console.log(error);
+        });
+
+        
+        for (var product of cart) {
+            var cartItem = ' ';
+            cartItem += "<div class=\"row\""
+            cartItem += "<div class=\"col-md-6\" id=\"prodImage\"> "
+            cartItem += "<img src=" + product.productImage[0] + " style=\"width:300px;height:300px;\"></img>"
+            cartItem += "<div class=\"col-md-6\" id=\"prodDetail\">"
+            cartItem += "<h1>" + product.productName + "</h1>"
+            cartItem += "<h4>" + "&#x20B9; " + product.price + "</h4>"
+            cartItem += "<button class=\"btn btn-lg btn-success\">Buy now</button>"
+            cartItem += "</div></div></div>"
         }
+        $("#showCart").append(cartItem);
     });
+
 }
 
 /*
-    desc: Adding to wishlist
+    @desc: display wishlist
 */
-function addToWishlist(id){
-    $.ajax({
-        type: "GET",
-        data : "json",
-        url: "../data/productData.json",
-        success : function(result){
-            for (var product of result)
-            { 
-                if(product.id == id)
-                {  
-                    wishlist.push(product);
-                    alert("Added to wishlist");
-                }
+function displayWishlist() {
+    //console.log(localStorage.wishlist);
+    $.getJSON("../data/productData.json", function (result) {
+        $.each(result.Products, function (data, wishlistItem) {
+            if (localStorage.wishlist == wishlistItem.id) {
+                cart.push(wishlistItem)
+                console.log(wishlist);
             }
-        },
-        error : function(error){
-            console.log(error);
+        });
+
+
+        for (var product of cart) {
+            var wishlistItem = ' ';
+            wishlistItem += "<div class=\"row\""
+            wishlistItem += "<div class=\"col-md-6\" id=\"prodImage\"> "
+            wishlistItem += "<img src=" + product.productImage[0] + " style=\"width:300px;height:300px;\"></img>"
+            wishlistItem += "<div class=\"col-md-6\" id=\"prodDetail\">"
+            wishlistItem += "<h1>" + product.productName + "</h1>"
+            wishlistItem += "<h4>" + "&#x20B9; " + product.price + "</h4>"
+            wishlistItem += "<button class=\"btn btn-lg btn-success\">Buy now</button>"
+            wishlistItem += "</div></div></div>"
+            
         }
+        $("#showCart").append(wishlistItem);
     });
+
 }
--- E-commerce-Website-master/js/category.js
+++ E-commerce-Website-master/js/category.js
@@ -15,11 +15,7 @@ $(document).ready(function () {
 
 			});
 
-			var menuData = "<li class=\"categories\"><a type=\"button\" >"
-				+ categoryItem.category + "</a><ul id=\"submenu\" >" + subMenu + "</ul></li>";
-
-			// var id = "ShowCategory";
-			// $(menuData).appendTo("#ShowCategory");
+			var menuData = "<li class=\"categories\"><a type=\"button\" >"+ categoryItem.category + "</a><ul id=\"submenu\" >" + subMenu + "</ul></li>";
 
 			$(menuData).appendTo($(".nav-stacked"));
 		});
@@ -67,7 +63,7 @@ var prod = [];
 function displayItems(category) {
 	//alert(category);
 	categoryProducts = [];
-	$.getJSON("../data/productData.json", function (result) {
+	$.get("../data/productData.json", function (result) {
 		$.each(result.Products, function (data, product) {
 
 			if (product.subCategory == category) {
@@ -81,54 +77,68 @@ function displayItems(category) {
 			productItem.setAttribute("id", product.id);
 			productItem.setAttribute("class", "col-md-4 col-sm-6 col-xs-12");
 			document.getElementById("productItems").appendChild(productItem);
-			//$( "productItems" ).replaceWith( productItem );
+			
 
-			var thumbnail = document.createElement("div");
+			var thumbnail = document.createElement("section");
 			thumbnail.setAttribute("class", "thumbnail");
 			thumbnail.setAttribute("id", "thumbnail" + product.id);
 			document.getElementById(product.id).appendChild(thumbnail);
-			//$( product.id ).replaceWith( thumbnail );
+			
 
 			var productImage = document.createElement("img");
 			productImage.setAttribute("src", product.productImage[0]);
 			document.getElementById("thumbnail" + product.id).appendChild(productImage);
-			//$("thumbnail" + product.id ).replaceWith(productImage);
+			
 
 			var brand = document.createElement("h4");
 			brand.setAttribute("id", product.brand + product.id);
 			document.getElementById("thumbnail" + product.id).append(brand);
 			document.getElementById(product.brand + product.id).innerHTML = product.brand;
-			//$( "thumbnail" + product.id ).replaceWith( brand );
+			
 
 			var name = document.createElement("p");
 			name.setAttribute("id", product.productName + product.id);
 			document.getElementById("thumbnail" + product.id).append(name);
 			document.getElementById(product.productName + product.id).innerHTML = product.productName;
-			//$("thumbnail" + product.id ).replaceWith( name );
+			
 
 			var price = document.createElement("p");
 			price.setAttribute("id", product.price + product.id);
 			document.getElementById("thumbnail" + product.id).append(price);
 			document.getElementById(product.price + product.id).innerHTML = "&#x20B9;" + " " + product.price;
-			//$( "thumbnail" + product.id).replaceWith(price );
+			
 
 			var wishlist = document.createElement("button");
 			wishlist.setAttribute("id", "wishlistBtn" + product.id);
 			wishlist.setAttribute("class", "btn btn-md btn-warning");
-			wishlist.setAttribute("onclick", "addToWishlist($(product.id));");
+			wishlist.addEventListener("click",function(e)
+			{	
+				localStorage.setItem("wishlist",product.id)
+				console.log(localStorage.wishlist);
+			})
 			document.getElementById("thumbnail" + product.id).appendChild(wishlist);
 			document.getElementById("wishlistBtn" + product.id).innerHTML = "<span class=\"glyphicon glyphicon-heart\">" + "Wishlist" + "</span>";
-			//$( "thumbnail" + product.id ).replaceWith(wishlist );
+			
 
 			var addToCart = document.createElement("button");
 			addToCart.setAttribute("id", "cartBtn" + product.id);
 			addToCart.setAttribute("class", "btn btn-md btn-info");
-			addToCart.setAttribute("onclick", "addToCart($(product.id));");
+			addToCart.addEventListener("click",function(e)
+			{	
+				//console.log(e.target.id)
+				localStorage.setItem("cart",product.id)
+				console.log(localStorage.cart);
+			})
 			document.getElementById("thumbnail" + product.id).appendChild(addToCart);
 			document.getElementById("cartBtn" + product.id).innerHTML = "<span class=\"glyphicon glyphicon-shopping-cart\">" + "Add to Cart" + "</span>";
-			//$("thumbnail" + product.id ).replaceWith(addToCart );
+			
 		}
 
 	});
 }
+
+
+
+
+
 
